16ce154f917171cc1b0cf8412a9d1f69b4b51b6d
David <david@gmail.com>
2025-03-28 18:09:30 -0500

    cambiio hoy28

--- src/signup.php
+++ src/signup.php
@@ -6,17 +6,38 @@ $lname  = $_POST['l_name'];
 $email  = $_POST['e_mail'];
 $passwd = $_POST['passw'];
 
-$sql = "INSERT INTO users(firstname, lastname, email, password)
-         VALUES('$fname','$lname','$email','$passwd')
-";
 
-$res = pg_query($conn, $sql);
+//$enc_pass = md5($passwd);
+$enc_pass = sha1($passwd);
 
-if($res){
-echo "user has been created succesfully";
+$sql_email_exist = "SELECT COUNT (email) as total FROM users WHERE EMAIL = '$email' LIMIT 1 "; 
+$res = pg_query($conn, $sql_email_exist);
+ if($res){
+   $row = pg_fetch_assoc($res);
+   if($row['total']>0){
+     echo "email already exist";} 
+ else{
+    $sql = "INSERT INTO users(firstname, lastname, email, password)
+    VALUES('$fname','$lname','$email','$enc_pass')
+    ";
 
-} else {
-echo "error";
+     $res = pg_query($conn, $sql);
+
+    if($res){
+     echo "user has been created succesfully";
+
+    } else {
+      echo "error";
+
+    }
+
+
+  } 
 
 }
+
+
+
+
+
 ?>
