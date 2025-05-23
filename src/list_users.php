df93a11834a97d66c579aaa899d17b81f531a08f
David <david@gmail.com>
2025-05-23 18:02:56 -0500

    clase 23

--- src/list_users.php
+++ src/list_users.php
@@ -14,10 +14,11 @@
     <table border='1' align="center">
         <tr> 
         <tr> 
-            <td>firstname</td>
-            <td>lastname</td>
-            <td>email</td>
-            <td>status</td>
+            <th>firstname</th>
+            <th>lastname</th>
+            <th>email</th>
+            <th>status</th>
+            <th>photo</th>
             <td>...</td>
             </tr>
         </tr> 
@@ -25,6 +26,7 @@
             //HERE CODE
             $sql = "
                 select 
+                id,
                 firstname,
                 lastname,
                 email,
@@ -43,10 +45,11 @@
                 echo"<td>". $row['lastname']."</td>";
                 echo"<td>". $row['email']."</td>";
                 echo"<td>". $row['status']."</td>";
+                echo"<td align='center'><img src='photo_users/photo_default.png' width='20'></td>";
                 echo"<td>";
                 echo "<a href=''><img src = 'icons/lapiz.png' width='30'>";
                 echo "<a href=''><img src = 'icons/lupa.png' width='30'>";   
-                echo "<a href=''><img src = 'icons/trash.jpg' width='30'>";
+                echo "<a href='https://127.0.0.1/schoolar/src/delete.php'><img src = 'icons/trash.jpg' width='30'>";
                 echo"</td>";
                 echo"</tr>";
 
